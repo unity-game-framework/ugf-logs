74fca78777e42cd24ac3a27b3f71464cfc31a715
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-09-27 00:21:37 +0300

    Update TestLog

--- Assets/UGF.Logs.Runtime.Tests/TestLog.cs
+++ Assets/UGF.Logs.Runtime.Tests/TestLog.cs
@@ -1,6 +1,74 @@
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
 namespace UGF.Logs.Runtime.Tests
 {
     public class TestLog
     {
+        private class CollectLogs : IDisposable
+        {
+            public List<string> Logs { get; } = new List<string>();
+
+            public CollectLogs()
+            {
+                Application.logMessageReceived += OnApplicationLogMessageReceived;
+            }
+
+            public void Dispose()
+            {
+                Application.logMessageReceived -= OnApplicationLogMessageReceived;
+            }
+
+            private void OnApplicationLogMessageReceived(string condition, string stacktrace, LogType type)
+            {
+                Logs.Add(condition);
+            }
+        }
+
+        [Test]
+        public void LogInfo()
+        {
+            using (var logs = new CollectLogs())
+            {
+                Log.Info(nameof(LogInfo));
+
+                Assert.Contains(nameof(LogInfo), logs.Logs);
+            }
+        }
+
+        [Test]
+        public void LogDebug()
+        {
+            using (var logs = new CollectLogs())
+            {
+                Log.Debug(nameof(LogDebug));
+
+                Assert.Contains(nameof(LogDebug), logs.Logs);
+            }
+        }
+
+        [Test]
+        public void LogWarning()
+        {
+            using (var logs = new CollectLogs())
+            {
+                Log.Warning(nameof(LogWarning));
+
+                Assert.Contains(nameof(LogWarning), logs.Logs);
+            }
+        }
+
+        // [Test]
+        // public void LogError()
+        // {
+        //     using (var logs = new CollectLogs())
+        //     {
+        //         Log.Error(nameof(LogError));
+        //
+        //         Assert.Contains(nameof(LogError), logs.Logs);
+        //     }
+        // }
     }
 }
