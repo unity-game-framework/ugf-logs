34cc0657bdd8e5b26ed74ffa323540a6ce27d8e4
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-09-04 16:03:09 +0300

    Add LogHandled

---
+++ Packages/UGF.Logs/Runtime/LogBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UGF.Logs.Runtime
+{
+    public abstract class LogBase : ILog
+    {
+        public void Message(string tag, object value)
+        {
+            if (string.IsNullOrEmpty(tag)) throw new ArgumentException("Value cannot be null or empty.", nameof(tag));
+            if (value == null) throw new ArgumentNullException(nameof(value));
+
+            OnMessage(tag, value);
+        }
+
+        protected abstract void OnMessage(string tag, object value);
+    }
+}
---
+++ Packages/UGF.Logs/Runtime/LogHandled.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UGF.Logs.Runtime
+{
+    public class LogHandled : LogBase
+    {
+        public ILogHandler Handler { get; }
+
+        public LogHandled(ILogHandler handler)
+        {
+            Handler = handler ?? throw new ArgumentNullException(nameof(handler));
+        }
+
+        protected override void OnMessage(string tag, object value)
+        {
+            Handler.Write(tag, value);
+        }
+    }
+}
