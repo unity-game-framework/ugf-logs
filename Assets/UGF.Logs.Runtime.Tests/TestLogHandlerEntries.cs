41068645ef18b53659ffdce071d1e9bf5b3d502d
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-09-04 16:58:43 +0300

    Add TestLogHandlerEntries

---
+++ Assets/UGF.Logs.Runtime.Tests/TestLogHandlerEntries.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace UGF.Logs.Runtime.Tests
+{
+    public class TestLogHandlerEntries
+    {
+        [Test]
+        public void Format()
+        {
+            var log = new LogHandled<LogHandlerEntries>(new LogHandlerEntries());
+
+            log.Info("Message 0", new { test = "Test" });
+            log.Warning("Message 1", new { test = "Test" });
+            log.Error("Message 2", new { test = "Test" });
+
+            string result = LogUtility.Format(log.Handler.Entries);
+
+            Assert.Pass(result);
+        }
+    }
+}
