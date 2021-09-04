b032ad8f23fcc4f4890371d6520836ae261d0465
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-09-04 16:58:35 +0300

    Add LogHandled<T>

---
+++ Packages/UGF.Logs/Runtime/LogHandled`1.cs
@@ -0,0 +1,11 @@
+﻿namespace UGF.Logs.Runtime
+{
+    public class LogHandled<THandler> : LogHandled where THandler : class, ILogHandler
+    {
+        public new THandler Handler { get { return (THandler)base.Handler; } }
+
+        public LogHandled(THandler handler) : base(handler)
+        {
+        }
+    }
+}
