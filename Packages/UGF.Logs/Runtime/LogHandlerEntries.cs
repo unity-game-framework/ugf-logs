fe0ee47779f38ff0212b80ee353f88c4e20802cc
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-09-04 16:28:20 +0300

    Add LogHandlerEntries

---
+++ Packages/UGF.Logs/Runtime/LogHandlerEntries.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UGF.Logs.Runtime
+{
+    public class LogHandlerEntries : LogHandlerBase
+    {
+        public List<LogEntry> Entries { get; } = new List<LogEntry>();
+        public bool LogStackTrace { get; set; } = true;
+
+        protected override void OnWrite(string tag, object value)
+        {
+            LogEntry entry;
+
+            if (LogStackTrace)
+            {
+                string stackTrace = value is Exception
+                    ? StackTraceUtility.ExtractStringFromException(value)
+                    : StackTraceUtility.ExtractStackTrace();
+
+                entry = new LogEntry(DateTime.Now.TimeOfDay, tag, value, stackTrace);
+            }
+            else
+            {
+                entry = new LogEntry(DateTime.Now.TimeOfDay, tag, value);
+            }
+
+            Entries.Add(entry);
+        }
+    }
+}
